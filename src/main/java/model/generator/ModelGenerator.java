a6317a4031ea2240a6fa619b86a7664b193bf911
baciar42 <patryk.karbownik98@gmail.com>
2021-12-29 12:51:40 +0100

    implemented classes, which supports input-output operations

--- src/main/java/model/generator/ModelGenerator.java
+++ src/main/java/model/generator/ModelGenerator.java
@@ -1,4 +1,44 @@
 package model.generator;
 
+import model.structures.Model;
+
+import java.util.Random;
+
 public class ModelGenerator {
+
+    private static int[][] generateMatrix(Random random, int maxEdgeWage, int nodesNumber)
+    {
+        int[][] matrix = new int[nodesNumber][nodesNumber];
+
+        for(int i = 0; i < nodesNumber; i++)
+            for(int j = i; j < nodesNumber; j++)
+            {
+                if(i == j)
+                    matrix[i][j] = 0;
+                else
+                {
+                    int value = random.nextInt(maxEdgeWage) + 1;
+
+                    matrix[i][j] = value;
+                    matrix[j][i] = value;
+                }
+            }
+
+        return matrix;
+    }
+
+    public static Model generateModel(int iterationsNumber, int nodesNumber, int maxEdgeWage, long seed)
+    {
+        Random random = new Random(seed);
+
+        int[][][] adjacencyMatrix = new int[iterationsNumber][nodesNumber][nodesNumber];
+
+        for(int i = 0; i < adjacencyMatrix.length; i++)
+            adjacencyMatrix[i] = generateMatrix(random, maxEdgeWage, nodesNumber);
+
+        Model model = new Model(nodesNumber, iterationsNumber);
+        model.setAdjacencyMatrix(adjacencyMatrix);
+
+        return model;
+    }
 }
--- src/main/java/model/io/JSONModelWriter.java
+++ src/main/java/model/io/JSONModelWriter.java
@@ -1,4 +1,21 @@
 package model.io;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.structures.Model;
+
+import java.io.File;
+import java.io.IOException;
+
 public class JSONModelWriter {
+
+    public static void writeToJSON(Model model, String pathToFile)
+    {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            mapper.writeValue(new File(pathToFile), model);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
