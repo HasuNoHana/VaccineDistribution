6439b4e4944952581a5e78b2d61c44dbe0020b0e
HasuNoHana <zusant22@gmail.com>
2022-01-09 11:41:03 +0100

    test

---
+++ src/test/java/model/simulatedAnnealing/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package model.simulatedAnnealing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    void helloWorld() {
+        System.out.printf("Hello World!");
+        Assertions.assertTrue(true);
+    }
+}
