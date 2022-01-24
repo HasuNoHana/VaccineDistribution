c20da4071d549ecd3f865da9d32bb6c937f7e3d2
HasuNoHana <zusant22@gmail.com>
2022-01-24 15:08:47 +0100

    fix error

--- src/test/java/model/simulatedAnnealing/SimulatorTest.java
+++ src/test/java/model/simulatedAnnealing/SimulatorTest.java
@@ -156,36 +156,58 @@ public class SimulatorTest {
     @Test
     public void shouldFindOptimalPathInSimpleGraphWithTreakyEdges() {
 //        given
-        Node node0 = new Node(0, 0, 0);
-        Node node1 = new Node(1, 0, 0);
-        Node node2 = new Node(2, 0, 0);
-        Node node3 = new Node(3, 0, 0);
+//        Node node0 = new Node(0, 0, 0);
+//        Node node1 = new Node(1, 0, 0);
+//        Node node2 = new Node(2, 0, 0);
+//        Node node3 = new Node(3, 0, 0);
+//
+//        GraphFactory graphFactory = new GraphFactory();
+//        int edgeWeight01 = 1;
+//        int edgeWeight12 = 2;
+//        int edgeWeight23 = 3;
+//        int edgeWeight02 = 4;
+//        int edgeWeight03 = 5;
+//        int edgeWeight13 = 1;
+//
+//        graphFactory.addNode(node0);
+//        graphFactory.addNode(node1);
+//        graphFactory.addNode(node2);
+//        graphFactory.addNode(node3);
+//        graphFactory.addWeight(0, 1, edgeWeight01);
+//        graphFactory.addWeight(0, 2, edgeWeight02);
+//        graphFactory.addWeight(0, 3, edgeWeight03);
+//        graphFactory.addWeight(1, 2, edgeWeight12);
+//        graphFactory.addWeight(1, 3, edgeWeight13);
+//        graphFactory.addWeight(2, 3, edgeWeight23);
+//
+//        GraphImpl graph = (GraphImpl) graphFactory.build();
+
+
+        ArrayList<Node> nodes = new ArrayList<>(Arrays.asList(new Node(0, 0, 0),
+                new Node(1, 0, 0),
+                new Node(2, 0, 0),
+                new Node(3, 0, 0)));
+
+        int[][] adjacencyMatrix = {
+                {0, 1, 4, 5},
+                {1, 0, 2, 1},
+                {4, 2, 0, 3},
+                {5, 1, 3, 0}
+        };
 
         GraphFactory graphFactory = new GraphFactory();
-        int edgeWeight01 = 1;
-        int edgeWeight12 = 2;
-        int edgeWeight23 = 3;
-        int edgeWeight02 = 4;
-        int edgeWeight03 = 5;
-        int edgeWeight13 = 1;
 
-        graphFactory.addNode(node0);
-        graphFactory.addNode(node1);
-        graphFactory.addNode(node2);
-        graphFactory.addNode(node3);
-        graphFactory.addWeight(0, 1, edgeWeight01);
-        graphFactory.addWeight(0, 2, edgeWeight02);
-        graphFactory.addWeight(0, 3, edgeWeight03);
-        graphFactory.addWeight(1, 2, edgeWeight12);
-        graphFactory.addWeight(1, 3, edgeWeight13);
-        graphFactory.addWeight(2, 3, edgeWeight23);
+        for (Node n : nodes)
+            graphFactory.addNode(n);
 
+        graphFactory.addAdjacencyMatrix(adjacencyMatrix);
         GraphImpl graph = (GraphImpl) graphFactory.build();
+
         GraphPathImpl simpleGraphPath = new GraphPathImpl();
-        simpleGraphPath.addToPath(node0);
-        simpleGraphPath.addToPath(node2, edgeWeight02);
-        simpleGraphPath.addToPath(node1, edgeWeight12);
-        simpleGraphPath.addToPath(node3, edgeWeight13);
+        simpleGraphPath.addToPath(nodes.get(0));
+        simpleGraphPath.addToPath(nodes.get(2), adjacencyMatrix[0][2]);
+        simpleGraphPath.addToPath(nodes.get(1), adjacencyMatrix[01][2]);
+        simpleGraphPath.addToPath(nodes.get(3), adjacencyMatrix[1][3]);
 
         GraphImpl graphSpyded = Mockito.spy(graph);
         when(graphSpyded.getRandomPath()).thenReturn(simpleGraphPath);
@@ -200,6 +222,6 @@ public class SimulatorTest {
 
         //then
         GraphPath optimalPath = simulationResult.getOptimalPath();
-        assertEquals(edgeWeight01 + edgeWeight12 + edgeWeight23, optimalPath.getSumOfWages());
+        assertEquals(adjacencyMatrix[0][1] + adjacencyMatrix[1][3] + adjacencyMatrix[3][2], optimalPath.getSumOfWages());
     }
 }
